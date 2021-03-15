b7f3f3d2c679bb0262a99d1963c8d195ded6d75e
Your <you@example.com>
2021-03-15 11:06:32 +0300

    feat: towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (typeof matrix == 'undefined') {
+    return [];
+  }
+  let forward = true
+  let towel = []
+  for (let row of matrix){
+      if (!forward){
+          row = row.reverse()
+      }
+      for (el of row){
+          towel.push(el)    
+      }
+      forward = !forward
+      
+      }
+  
+  return towel;
+  
 }
